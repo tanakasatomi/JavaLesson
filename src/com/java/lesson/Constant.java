f0543daa8418098e6faec6c7405def072a8c337b
田中 聡美 <s_tanaka@sharecrest.com>
2023-06-05 11:27:38 +0900

    Java入門 - Lesson1 - Constant 追加:gitにcommitしました

---
+++ src/com/java/lesson/Constant.java
@@ -0,0 +1,10 @@
+package com.java.lesson;
+
+public class Constant {
+	public static void main(String[] args){
+		final int ADULT_AGE = 20;
+		ADULT_AGE = 25;
+		System.out.println(ADULT_AGE);
+	}
+
+}
