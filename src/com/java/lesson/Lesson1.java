9ca6108df7d0830dbaf73b97274d0e5350bbf9c5
田中 聡美 <s_tanaka@sharecrest.com>
2023-06-05 11:38:13 +0900

    Java入門 - Lesson1 - Lesson1追加:gitにcommitしました

---
+++ src/com/java/lesson/Lesson1.java
@@ -0,0 +1,16 @@
+package com.java.lesson;
+
+public class Lesson1 {
+	public static void main(String[] args) {
+		//(1)
+		System.out.println("Java入門の練習問題を始めます。");
+		
+		//(2)
+		int TEISU = 15;
+		int hensu =10;
+		
+		hensu = 50;
+		System.out.println(hensu);
+	}
+
+}
