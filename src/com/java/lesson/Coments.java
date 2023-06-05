d6108add61633b0666a161accc3065e2792e7f55
田中 聡美 <s_tanaka@sharecrest.com>
2023-06-05 11:00:10 +0900

    Java入門 - Lesson1 - Coments追加:gitにcommitしました

---
+++ src/com/java/lesson/Coments.java
@@ -0,0 +1,16 @@
+package com.java.lesson;
+
+public class Coments {
+		/**
+		 * Comentsクラス
+		 * このクラスは、コメントの書き方を学ぶためのクラスです。
+		 * @author s_tanaka
+		 * @param　引数(パラメータ)についての説明
+		 * @return 戻り値についての説明
+		 * @throws 例外処理についての説明
+		 */
+	public static void main(String[] args) {
+		System.out.println("JavaDocを記載しました。");
+	}
+
+}
